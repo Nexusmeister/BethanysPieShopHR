fe33b96d309f32dff68f5ff803bfb0b0a544172c
Kaltenbach, Robin <robin@cskaltenbach.de>
2025-02-17 19:11:29 +0100

    RenderFragments

---
+++ BethanysPieShopHRM.Server/Components/ProfilePicture.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Server.Components;
+
+public sealed partial class ProfilePicture
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+}
